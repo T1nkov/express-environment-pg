5e8543552e932116fe5ab60ac5c2c5c90f59e430
Dmitry Tinkov <mrfredyon@gmail.com>
2023-12-29 23:19:09 +0300

    dz

--- src/service/skill.service.js
+++ src/service/skill.service.js
@@ -9,4 +9,7 @@ async function createNewSkill(label, category, priority) {
   const data = await createNewSkillDB(label, category, priority);
   return data;
 }
+
+async function updateSkill(id, label, category, priority) {}
+
 module.exports = { getAllSkill, createNewSkill };
